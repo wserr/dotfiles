46f7e80e087f0ab8d321f64efa6b2a1b638ba1a0
Willem Serruys <willem.serruys@external.atlascopco.com>
2026-02-09 20:18:34 +0100

    Updated

--- fish/completions/kcong.fish
+++ fish/completions/kcong.fish
@@ -1,1 +1,9 @@
-complete -f -c kcong -s b -l bootstrap-server -a "(cat ~/.config/kafka/bootstrap-servers)" -d "Kafka bootstrap server"
+#kafka-consumer-groups.sh --bootstrap-server pkc-57q33g.westeurope.azure.confluent.cloud:9092 --command-config "./admin.properties.confluent"  --describe --all-groups > consumer-groups.txt
+complete -c kcong -f
+
+complete -c kcong -l bootstrap-server -a "(cat ~/.config/kafka/bootstrap-servers)" -d "Kafka bootstrap server"
+complete -c kcong -l command-config -a "(find /home/willem/.config/kafka/clusters -type f)" -d "properties file"
+
+ls -d $PWD/*
+
+
---
+++ fish/functions/kconc.fish
@@ -0,0 +1,2 @@
+#kafka-consumer-groups.sh --bootstrap-server pkc-57q33g.westeurope.azure.confluent.cloud:9092 --command-config "./admin.properties.confluent"  --describe --all-groups > consumer-groups.txt
+
